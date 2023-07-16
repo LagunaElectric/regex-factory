26cd28f1411cfc4c4dc98e870014c80c8a258f51
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2023-07-15 23:29:40 -0400

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/devtools',
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/color-mode',
+    'nuxt-mongoose'
+  ]
+})
