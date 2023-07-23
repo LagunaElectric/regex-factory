2b08b8addcdd234d7485414bf3ab8d4a4d9a13c6
LagunaElectric <6085143+LagunaElectric@users.noreply.github.com>
2023-07-23 14:08:19 -0400

    improve header responsiveness

--- components/Header.vue
+++ components/Header.vue
@@ -2,20 +2,20 @@
 
 <template>
   <div
-    class="dark:bg-primary-900 dark:text-primary-icon max-h-16 items-center flex justify-center lg:justify-between p-4"
+    class="dark:bg-primary-900 dark:text-primary-icon max-h-16 items-center flex justify-center lg:justify-between p-4 relative"
   >
     <span class="text-4xl font-bold self-center">
       <span class="bg-gradient-to-br bg-clip-text text-transparent from-red-500 to-orange-300 italic">RegEx</span
       >Factory
     </span>
-    <div class="lg:flex gap-1 hidden">
+    <div class="sm:flex gap-1 absolute lg:static right-4 top-4 hidden">
       <NuxtLink to="https://www.github.com/LagunaElectric" target="_blank">
-        <IconButton class="text-primary-icon w-8 h-8 grow hover:bg-primary-active" name="brandico:github" />
+        <IconButton class="text-primary-icon w-8 h-8 rounded-sm grow hover:bg-primary-active" name="brandico:github" />
       </NuxtLink>
       <NuxtLink to="https://www.paypal.com/paypalme/my/profile" target="_blank">
-        <IconButton class="text-primary-icon w-8 h-8 grow hover:bg-primary-active" name="simple-icons:paypal" />
+        <IconButton class="text-primary-icon w-8 h-8 rounded-sm grow hover:bg-primary-active" name="simple-icons:paypal" />
       </NuxtLink>
-      <IconButton class="text-primary-icon w-8 h-8 grow hover:bg-primary-active" name="mdi:information-outline" />
+      <IconButton class="text-primary-icon w-8 h-8 rounded-sm grow hover:bg-primary-active" name="mdi:information-outline" />
     </div>
   </div>
 </template>
